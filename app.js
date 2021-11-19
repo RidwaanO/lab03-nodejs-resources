a44e8164efb0a6e9b75509c18a0c7a20083c5738
Ridwaan Omar <rao5@hw.ac.uk>
2021-11-19 15:03:37 +0400

    done

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//creating app
+const app = express();
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+ console.log(`Cart app listening at http://localhost:${port}`);
+});
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+ res.render('index'); //no need for ejs extension
+});
+app.get('/contacts', (req, res) => {
+    res.render('contacts');
+   });
+   
